c0e27809aea902ecbf7c309494e14464378011f6
권세명 <3pkwon@gmail.com>
2023-03-21 17:31:47 +0900

    console

---
+++ W2Day2/variables.js
@@ -0,0 +1,18 @@
+const TRUE = true;
+const FALSE = false;
+
+// // && AND
+console.log(TRUE && TRUE);
+console.log(TRUE && FALSE);
+console.log(FALSE && TRUE);
+console.log(FALSE && FALSE);
+
+// || OR
+console.log(TRUE || TRUE);
+console.log(TRUE || FALSE);
+console.log(FALSE || TRUE);
+console.log(FALSE || FALSE);
+
+// ! NOT
+console.log(!TRUE);
+console.log(!FALSE);
